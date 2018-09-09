ee280a81e01e2d4338b1f4a7388f91936ff50560
Mahasen Sooriyabandara <mahasen.sooriyabandara@gmail.com>
2018-09-09 10:47:13 +1000

    Added routes to API for marginal distributions

--- api/main.py
+++ api/main.py
@@ -13,6 +13,32 @@ def calculate_relative_risk(sa3,sex,major_group,family_situation):
 
     return np.sqrt(risk_factor_b25**2 + risk_factor_g57a**2) / np.sqrt(2)
 
+
+### Marginal distributions
+def calculate_prob_noncompliance_sa3(sa3,use_norm):
+    out = data['b25_posterior_SA3_only'][sa3]
+    if use_norm == True:
+        out = out/max(data['b25_posterior_SA3_only'].values())
+    return out
+
+def calculate_prob_noncompliance_family(sa3,use_norm):
+    out = data['b25_posterior_FS_only'][sa3]
+    if use_norm == True:
+        out = out/max(data['b25_posterior_FS_only'].values())
+    return out
+
+def calculate_prob_noncompliance_sex(sa3,use_norm):
+    out = data['g57a_posterior_sex_only'][sa3]
+    if use_norm == True:
+        out = out/max(data['g57a_posterior_sex_only'].values())
+    return out
+
+def calculate_prob_noncompliance_majGroup(sa3,use_norm):
+    out = data['g57a_posterior_majGroup_only'][sa3]
+    if use_norm == True:
+        out = out/max(data['g57a_posterior_majGroup_only'].values())
+    return out
+
 ### YEAH LETS JUST DO THAT.
 
 from flask import Flask, request
@@ -33,6 +59,47 @@ def calculaterisk():
     familySituation = request.args.get('familySituation')
     riskfactor = calculate_relative_risk(int(sa3), sex, int(majorGroup), familySituation)
     return json.dumps({"riskfactor" : riskfactor})
+
+@app.route("/calculate_prob_sa3")
+def calculate_prob_sa3():
+    help = request.args.get('help')
+    if (help == "me"):
+        return "{}?sa3=80105&useNorm=True".format(request.base_url)
+    sa3 = request.args.get('sa3')
+    useNorm = request.args.get('useNorm')
+    prob = calculate_prob_noncompliance_sa3(int(sa3),useNorm)
+    return json.dumps({"probability" : prob})
+
+@app.route("/calculate_prob_family")
+def calculate_prob_family():
+    help = request.args.get('help')
+    if (help == "me"):
+        return "{}?familySituation=Single%20with%20Dependants&useNorm=True".format(request.base_url)
+    familySituation = request.args.get('familySituation')
+    useNorm = request.args.get('useNorm')
+    prob = calculate_prob_noncompliance_family(familySituation,useNorm)
+    return json.dumps({"probability" : prob})
+
+@app.route("/calculate_prob_sex")
+def calculate_prob_sex():
+    help = request.args.get('help')
+    if (help == "me"):
+        return "{}?sex=Female&useNorm=True".format(request.base_url)
+    sex = request.args.get('sex')
+    useNorm = request.args.get('useNorm')
+    prob = calculate_prob_noncompliance_sex(sex,useNorm)
+    return json.dumps({"probability" : prob})
+
+
+@app.route("/calculate_prob_majorGroup")
+def calculate_prob_occupation():
+    help = request.args.get('help')
+    if (help == "me"):
+        return "{}?majorGroup=3&useNorm=True".format(request.base_url)
+    majorGroup = request.args.get('majorGroup')
+    useNorm = request.args.get('useNorm')
+    prob = calculate_prob_noncompliance_majGroup(int(majorGroup),useNorm)
+    return json.dumps({"probability" : prob})
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5500, threaded=True, debug=True)
